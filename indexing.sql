78663912348b3074fdb092dea9c4a5e90b94716a
Chaz <cmtlum73@gmail.com>
2016-01-20 17:21:37 -1000

    adds indexing on table creation

--- indexing.sql
+++ indexing.sql
@@ -21,4 +21,12 @@ CREATE DATABASE indexed_cars;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
 
-SELECT COUNT(*) FROM car_models;
+SELECT DISTINCT make_title FROM car_models WHERE make_code='LAM';
+
+SELECT DISTINCT model_title FROM car_models WHERE make_code='NISSAN' AND model_code='GT-R';
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code='LAM';
+
+SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
+
+SELECT * FROM car_models WHERE year=2010;
--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,4 +9,10 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
+CREATE INDEX makeIndex ON car_models (make_code);
+
+CREATE INDEX makeANDmodelIndex ON car_models (make_code, model_code);
+
+CREATE INDEX yearIndex ON car_models (year);
